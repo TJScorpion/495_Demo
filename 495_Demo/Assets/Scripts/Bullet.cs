86edd5b6b296405fb1a91725106a997add07a93d
TJScorpion <tjscorpion93@gmail.com>
2017-01-28 15:28:20 -0800

    First Commit

---
+++ 495_Demo/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private float speed = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		transform.Translate(Vector3.up * (Time.deltaTime*speed));
+	}
+}
---
+++ 495_Demo/Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShip : MonoBehaviour {
+
+	public float health;
+	private float speed;
+
+	public GameObject bullet_Prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		//Get input from keyboard to move player ship forward
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) 
+		{
+			speed = 10;
+			transform.Translate (Vector3.up * (Time.deltaTime * speed));
+		} else 
+		{
+			speed = 0;
+		}
+
+		//Get input from keyboard to move player ship backward
+		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) 
+		{
+			speed = 10;
+			transform.Translate (Vector3.down * (Time.deltaTime * speed));
+		} else 
+		{
+			speed = 0;
+		}
+
+		//Get input from keyboard to move player ship right
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) 
+		{
+			speed = 10;
+			transform.Translate (Vector3.right * (Time.deltaTime * speed));
+		} else 
+		{
+			speed = 0;
+		}
+
+		//Get input from keyboard to move player ship left
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			speed = 10;
+			transform.Translate (Vector3.left * (Time.deltaTime * speed));
+		} else 
+		{
+			speed = 0;
+		}
+		//Get input and start Fire method
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Fire ();
+		}
+	}//End Update
+
+	//Creates an instance of a bullet at the ships location
+	void Fire()
+	{
+		GameObject go;
+		go = (GameObject)Instantiate(bullet_Prefab, this.transform.position, transform.rotation);
+	}
+}
